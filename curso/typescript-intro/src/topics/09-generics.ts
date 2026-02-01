9ee2f4dd5a3947334849cc8daea4007bf272300d
rimcodedev <114328959+rimcodedev@users.noreply.github.com>
2026-02-01 10:11:08 +0100

    Example of genercis in TypeScript

--- curso/typescript-intro/src/topics/09-generics.ts
+++ curso/typescript-intro/src/topics/09-generics.ts
@@ -1,5 +1,13 @@
 
 
-export function whatsMyType(argument: any){
+export function whatsMyType<T>(argument: T): T {
     return argument;
 }   
+
+const amIString = whatsMyType<string>("Hello World");
+const amINumber = whatsMyType<number>(100);
+const amIArray = whatsMyType([1,2,3,4,5]);
+
+console.log(amIString.split(' '));
+console.log(amINumber.toFixed(2));
+console.log(amIArray.join('-'));
